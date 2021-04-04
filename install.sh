20ae54e13cc97d1739dbd5e77e5d52d06d9806dc
0xC4SE <761779465@qq.com>
2021-04-04 12:50:07 +0800

    fzfautoinstallnew

--- install.sh
+++ install.sh
@@ -3,76 +3,7 @@ myzshrc=${HOME}/.zshrc
 if [ ! -f "$myzshrc" ];then
     cp ~/fuzzfilefinder/.zshrc ${HOME}/.zshrc
 else
-    echo "
-###
-###FZF
-###
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-#export FZF_DEFAULT_OPTS='--bind=ctrl-t:top,change:top --bind ctrl-e:down,ctrl-u:up'
-#export FZF_TMUX=1
-export FZF_DEFAULT_OPTS='--bind=ctrl-k:down,ctrl-i:up --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_COMPLETION_TRIGGER='\'
-export FZF_TMUX_HEIGHT='80%'
-export FZF_PREVIRE_COMMAND='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500'
-
-_fzf_fpath=~/fzf
-fpath+=$_fzf_fpath
-autoload -U $_fzf_fpath/*(.:t)
-unset _fzf_fpath
-
-fzf-redraw-prompt() {
-	local precmd
-	for precmd in $precmd_functions; do
-		$precmd
-	done
-	zle reset-prompt
-}
-zle -N fzf-redraw-prompt
-
-zle -N fzf-find-widget
-bindkey '^p' fzf-find-widget
-
-fzf-cd-widget() {
-	local tokens=(${(z)LBUFFER})
-	if (( $#tokens <= 1 )); then
-		zle fzf-find-widget 'only_dir'
-		if [[ -d $LBUFFER ]]; then
-			cd $LBUFFER
-			local ret=$?
-			LBUFFER=
-			zle fzf-redraw-prompt
-			return $ret
-		fi
-	fi
-}
-zle -N fzf-cd-widget
-bindkey '^t' fzf-cd-widget
-
-fzf-history-widget() {
-	local num=$(fhistory $LBUFFER)
-	local ret=$?
-	if [[ -n $num ]]; then
-		zle vi-fetch-history -n $num
-	fi
-	zle reset-prompt
-	return $ret
-}
-zle -N fzf-history-widget
-bindkey '^R' fzf-history-widget
-
-fif() {
-  if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
-  rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
-}
-
-find-in-file() {
-	grep --line-buffered --color=never -r "" * | fzf
-}
-zle -N find-in-file
-bindkey '^f' find-in-file
-
-[[ -s ~/.autojump/etc/profile.d/autojump.sh  ]] && . ~/.autojump/etc/profile.d/autojump.sh" >> ${HOME}/.zshrc
+    cat ~/fuzzfilefinder/.zshrc >> ${HOME}/.zshrc
 fi
 
 fuzzfilefinder={HOME}/fzf/
@@ -89,7 +20,7 @@ autojump={HOME}/autojump/
 if [ ! -d "$autojump" ];then
     cp -R ~/fuzzfilefinder/autojump/ ${HOME}/;
 fi
-cp -R ~/fuzzfilefinder/.fzf.zsh/ ${HOME}/;
+cp ~/fuzzfilefinder/.fzf.zsh ${HOME}/;
 cd ~;cd .fzf/;./install;
 cd ~;cd autojump/;./install.py;
 
